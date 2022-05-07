c6b036742eba1b023681c5c70f608df35fc266b9
apawate <apawate739@student.fuhsd.org>
2022-05-07 11:38:45 -0700

    The initial photo finish is ready! Now I just need to find a way to save it and add timestamps.

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import numpy as np
 import cv2 as cv
 
 race = input("Enter the video file name: ")
-width = int(input("How many seconds should the finish cover?"))
+width = int(input("How many seconds should the finish cover? "))
 width = width * 30
 col = width
 cap = cv.VideoCapture(race)
@@ -13,7 +13,8 @@ while cap.isOpened():
         break
     #cv.imshow('frame', frame)
     height = frame.shape[0]
-    output = np.zeros((height,width,3), np.uint8)
+    if col == width:
+        output = np.zeros((height, width, 3), np.uint8)
     snippet = frame[0:height-1, 0:1]
     output[0:height-1, col-1:col] = snippet
     cv.imshow('zeroes', output)
