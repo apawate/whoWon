62ff25c9df5af093e7b0268aa78bc3cea6bf6571
apawate <apawate739@student.fuhsd.org>
2022-05-07 11:33:41 -0700

    Got a thin line to move across the screen. Finally.

--- main.py
+++ main.py
@@ -2,13 +2,22 @@ import numpy as np
 import cv2 as cv
 
 race = input("Enter the video file name: ")
+width = int(input("How many seconds should the finish cover?"))
+width = width * 30
+col = width
 cap = cv.VideoCapture(race)
 
 while cap.isOpened():
     ret, frame = cap.read()
     if not ret:
         break
-    cv.imshow('frame', frame)
+    #cv.imshow('frame', frame)
+    height = frame.shape[0]
+    output = np.zeros((height,width,3), np.uint8)
+    snippet = frame[0:height-1, 0:1]
+    output[0:height-1, col-1:col] = snippet
+    cv.imshow('zeroes', output)
+    col = col - 1
     if cv.waitKey(1) == ord('q'):
         break
 
