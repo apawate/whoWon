a03716c04e8ac7f313ab4a284c13638d36ca1695
apawate <apawate739@student.fuhsd.org>
2022-06-04 16:44:28 -0700

    Made the webcam using OPENCV and Flask. The webcam device will now need to actually run Python, but that shouldn't be too much of a problem.

--- webcam.py
+++ webcam.py
@@ -1,25 +1,29 @@
-from flask import Flask, render_template_string, request
-
+from flask import Flask, render_template_string, request, Response
+import cv2 as cv
 
 app = Flask('app')
 
+camera = cv.VideoCapture(0)
+
+def gen_frames():
+    while True:
+        success, frame = camera.read()
+        if not success:
+            break
+        else:
+            ret, buffer = cv2.imencode('.jpg', frame)
+            frame = buffer.tobytes()
+            yield (b'--frame\r\n'b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
 @app.route('/')
 def index():
-    return render_template_string('''
-    <video id="player" controls autoplay></video>
-    <script>
-        var player = document.getElementById('player');
+    return render_template('index.html')
 
-        const constraints = {
-            video: true,
-        };
 
-    if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-    navigator.mediaDevices.getUserMedia(constraints).then(function(stream) {
-        player.srcObject = stream;
-        player.play();
-    });
-    }
-    </script>''')
+@app.route('/video_feed')
+def video():
+    return Response(gen_frames(), mimetype='multipart/x-mixed-replace;boundary=frame')
+
+
 
 app.run('0.0.0.0', port=3000)
