a1f68a9d102e2e1abcf16e9c5b9b81000dcf7a1e
apawate <apawate739@student.fuhsd.org>
2022-06-07 15:17:56 -0700

    Trying something new because OPENCV can't do multiple requests.

--- testing.py
+++ testing.py
@@ -1,9 +1,18 @@
 import cv2
 import numpy as np
+from urllib.request import urlopen
 
-camera = cv2.VideoCapture("udp://10.0.1.12:3000")
+
+def url_to_image(url):
+
+    resp = urlopen(url)
+    image = np.asarray(bytearray(resp.read()), dtype="uint8")
+    image = cv2.imdecode(image, cv2.IMREAD_COLOR)
+
+    return image
+
 
 while True:
-    ret, frame = camera.read()
+    frame = url_to_image("http://10.0.1.15:3000")
     cv2.imshow('Webcam', frame)
 
