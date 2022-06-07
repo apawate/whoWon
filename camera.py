b2b883f6eba7df728f268cb4b637492f4e6326b4
apawate <apawate739@student.fuhsd.org>
2022-06-07 15:44:16 -0700

    Desperately trying anything that works. WHY DOES OPENCV NOT SUPPORT THREADS :(

--- camera.py
+++ camera.py
@@ -1,35 +1,29 @@
-import io
-from PIL import Image
-import select
-import v4l2capture
+import os
+import cv2
 from base_camera import BaseCamera
 
 
 class Camera(BaseCamera):
-    """Requires python-v4l2capture module: https://github.com/gebart/python-v4l2capture"""
+    video_source = 0
 
-    video_source = "/dev/video0"
+    def __init__(self):
+        if os.environ.get('OPENCV_CAMERA_SOURCE'):
+            Camera.set_video_source(int(os.environ['OPENCV_CAMERA_SOURCE']))
+        super(Camera, self).__init__()
+
+    @staticmethod
+    def set_video_source(source):
+        Camera.video_source = source
 
     @staticmethod
     def frames():
-        video = v4l2capture.Video_device(Camera.video_source)
-        # Suggest an image size. The device may choose and return another if unsupported
-        size_x = 640
-        size_y = 480
-        size_x, size_y = video.set_format(size_x, size_y)
-        video.create_buffers(1)
-        video.queue_all_buffers()
-        video.start()
-        bio = io.BytesIO()
+        camera = cv2.VideoCapture(Camera.video_source)
+        if not camera.isOpened():
+            raise RuntimeError('Could not start camera.')
 
-        try:
-            while True:
-                select.select((video,), (), ())  # Wait for the device to fill the buffer.
-                image_data = video.read_and_queue()
-                image = Image.frombytes("RGB", (size_x, size_y), image_data)
-                image.save(bio, format="jpeg")
-                yield bio.getvalue()
-                bio.seek(0)
-                bio.truncate()
-        finally:
-            video.close()
+        while True:
+            # read current frame
+            _, img = camera.read()
+
+            # encode as a jpeg image and return it
+            yield cv2.imencode('.jpg', img)[1].tobytes()
