a8d0b279bd41407591844aa1fe9da954ea1b56b7
apawate <apawate739@student.fuhsd.org>
2022-06-09 15:51:47 -0700

    And...it works, if you can turn print statements into images somehow. That's what I need to do.

--- testing.py
+++ testing.py
@@ -1,8 +1,6 @@
 import cv2
-import urllib.request
-import numpy as np
-req = urllib.request.urlopen('10.0.1.12:3000')
-arr = np.asarray(bytearray(req.read()), dtype=np.uint8)
-img = cv2.imdecode(arr, -1)
-cv2.imshow('Webcam', img)
-if cv2.waitKey() & 0xff == 27: quit()
+stream = cv2.VideoCapture("http://10.0.1.12:3000")
+
+while True:
+    img = stream.read()
+    print(img)
