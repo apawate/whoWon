b906746d57b67c2c913450ffdf368bffa349f843
apawate <apawate739@student.fuhsd.org>
2022-06-08 17:57:01 -0700

    I can open website images now, but the stream doesn't work.

--- testing.py
+++ testing.py
@@ -1,8 +1,8 @@
 import cv2
+import urllib.request
 import numpy as np
-
-camera = cv2.VideoCapture("udp://10.0.1.12:3000")
-
-while True:
-    ret, frame = camera.read()
-    cv2.imshow('Webcam', frame)
+req = urllib.request.urlopen('10.0.1.12:3000')
+arr = np.asarray(bytearray(req.read()), dtype=np.uint8)
+img = cv2.imdecode(arr, -1)
+cv2.imshow('Webcam', img)
+if cv2.waitKey() & 0xff == 27: quit()
