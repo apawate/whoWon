147a2331044f2e4e2de9ebc71655a08cdd1d3b7f
apawate <apawate739@student.fuhsd.org>
2022-06-09 17:15:29 -0700

    IT WORKS!!!!!!!!!!!!!!!!!!!!!!!!!!!

--- testing.py
+++ testing.py
@@ -1,6 +1,13 @@
 import cv2
+import numpy as np
+
 stream = cv2.VideoCapture("http://10.0.1.12:3000")
 
 while True:
-    img = stream.read()
-    print(img)
+    state, img = stream.read()
+    cv2.imshow('Webcam', img)
+    if cv2.waitKey(1) == ord('q'):
+        break
+
+stream.release()
+cv2.destroyAllWindows()
