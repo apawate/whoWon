cd16de2dff242bf1658b56d7e3bbd20cae486a8d
apawate <apawate739@student.fuhsd.org>
2022-06-12 18:27:17 -0700

    There is now only one webcam program - the one that works.

--- webcam.py
+++ webcam.py
@@ -1,25 +1,18 @@
-from flask import Flask, render_template, request, Response
-import cv2 
+from flask import Flask, render_template, Response
+from camera import Camera
 
-app = Flask('app')
+app = Flask(__name__)
 
-camera = cv2.VideoCapture(0)
-
-def gen_frames():
+def gen(camera):
     while True:
-        success, frame = camera.read()
-        if not success:
-            break
-        else:
-            ret, buffer = cv2.imencode('.jpg', frame)
-            frame = buffer.tobytes()
-            yield (b'--frame\r\n'b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
-
+        frame = camera.get_frame()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
 @app.route('/')
-def video():
-    return Response(gen_frames(), mimetype='multipart/x-mixed-replace;boundary=frame')
+def video_feed():
+    return Response(gen(Camera()),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
 
-
-
-app.run('0.0.0.0', port=3000)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
